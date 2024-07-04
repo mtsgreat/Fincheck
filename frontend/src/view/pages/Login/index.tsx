4ba35c6cbc734b549c3fa26187cd055c2ec58fa6
Mateus <mtsgreat@gmail.com>
2024-07-04 19:59:11 -0300

    finish layout login and register screen

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
 export function Login(){
     return (
-        <h1>Login Page</h1>
+        <>
+            <header className="flex flex-col items-center gap-4 text-center">
+                <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+                    Entre em sua conta
+                </h1>
+
+                <p className="space-x-2">
+                    <span className="text-gray-700 tracking-[-0.5px] font-normal">
+                        Novo por aqui?
+                    </span>
+                    <Link
+                    to="/register"
+                    className="tracking-[-0.5px] text-teal-600 font-medium"
+                    >
+                    Crie uma conta
+                    </Link>
+                </p>
+            </header>
+
+            <form className="mt-[60px] flex flex-col gap-4">
+                <Input type="email" placeholder="E-mail" name="email" />
+                <Input type="password" placeholder="Senha" name="senha" />
+
+                <Button type="submit" className="mt-2">
+                    Entrar
+                </Button>
+            </form>
+        </>
     )
 }
