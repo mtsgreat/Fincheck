d09b9a8dbb454eb3ecff0ced166680f7b1dcb9de
Mateus <mtsgreat@gmail.com>
2024-08-07 10:39:03 -0300

    create modal and dropdown menu logout

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -1,12 +1,29 @@
-export function UserMenu( {onSignout}: any){
+import { ExitIcon } from "@radix-ui/react-icons";
+import { DropdownMenu } from "./DropdownMenu";
+
+
+
+export function UserMenu(){
     return (
-        <div
-        className="bg-teal-100 rounded-full w-12 h-12 flex items-center justify-center"
-        onClick={onSignout}
-        >
-            <span className="text-sm tracking-[-0.5px] text-teal-600 font-medium">
-               MA
-            </span>
-        </div>
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+            <div
+                className="bg-teal-100 rounded-full w-12 h-12 flex items-center justify-center"
+                >
+                    <span className="text-sm tracking-[-0.5px] text-teal-600 font-medium">
+                    MA
+                    </span>
+                </div>
+            </DropdownMenu.Trigger>
+
+
+              <DropdownMenu.Content className="w-32">
+                <DropdownMenu.Item className="flex items-center justify-between" >
+                    Sair
+                    <ExitIcon className="w-4 h-4"/>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+
+        </DropdownMenu.Root>
     )
 }
--- frontend/src/view/pages/Dashboard/index.tsx
+++ frontend/src/view/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "../../../app/hooks/useAuth"
+
 import logoGreen from "../../../assets/logo-green.svg"
 import { UserMenu } from "../../components/UserMenu"
 import { Accounts } from "./components/Accounts"
@@ -6,14 +6,14 @@ import { DashboardProvider } from "./components/DashboardContext"
 import { Transactions } from "./components/Transactions"
 
 export function Dashboard(){
-    const { signout } = useAuth()
+
     return (
         <DashboardProvider>
         <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
 
             <header className="h-12 flex items-center justify-between">
                 <img className="h-6" src={logoGreen} alt="" />
-                <UserMenu onSignout={signout}/>
+                <UserMenu/>
             </header>
 
             <main className="flex-1 flex flex-col md:flex-row gap-4 max-h-full">
